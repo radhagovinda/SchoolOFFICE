33800df5b76e1582f54d3f185bbbff2625d8a3d5
radhagovinda <brainscape@live.com>
2014-07-19 01:37:54 +0530

    updated events and table relations

---
+++ visual/footer.php
@@ -0,0 +1,13 @@
+</td>
+  </tr>
+  <tr>
+    <td><table width="100%" border="0" cellspacing="5" cellpadding="0">
+      <tr>
+        <td align="center" bgcolor="##4F5257FFFFFF"><span class="style7">&copy; Shiksha Sutram</span></td>
+      </tr>
+    </table></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+</body>
+</html>
